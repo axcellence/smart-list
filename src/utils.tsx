7eff981e2befff24403c1e759308205ea3557aa8
Axl Crawford <hi@axlcrawford.com>
2023-06-12 15:22:35 +0100

    Update getItemCategory function to include a response for when categorization is uncertain

--- src/utils.tsx
+++ src/utils.tsx
@@ -13,7 +13,7 @@ export async function getItemCategory(item: string) {
       {
         role: "system",
         content:
-          'You are to act as categorisation system. You will be given an item and you must categorise it into one of the following categories: "Fruits and vegetables", "Dairy and eggs", "Meat and poultry", "Fish and seafood", "Grains, pasta, and rice", "Breads and bakery", "Canned and jarred goods", "Frozen foods", "Snacks and sweets", "Condiments and sauces", "Spices and seasonings", "Beverages", "Breakfast items", "Deli and prepared foods", "Cooking oils and fats", "Baking supplies", "Health foods and supplements", "Baby and toddler products", "Pet food and supplies", "Household and cleaning products", "Personal care items", "Office supplies", "Other". Only ever use the categories listed above, in the format listed above. If you are unsure, please use the category "Other".',
+          'You are to act as categorisation system. You will be given an item and you must categorise it into one of the following categories: "Fruits and vegetables", "Dairy and eggs", "Meat and poultry", "Fish and seafood", "Grains, pasta, and rice", "Breads and bakery", "Canned and jarred goods", "Frozen foods", "Snacks and sweets", "Condiments and sauces", "Spices and seasonings", "Beverages", "Breakfast items", "Deli and prepared foods", "Cooking oils and fats", "Baking supplies", "Health foods and supplements", "Baby and toddler products", "Pet food and supplies", "Household and cleaning products", "Personal care items", "Office supplies", "Other". Only ever use the categories listed above, in the format listed above. If you are unsure, respond with simply: "Other".',
       },
       { role: "user", content: item },
     ],
